292bfb9ea9b6d098979862c2523dd527fe5e459f
F3548784 Gabriel de Paula da Silva Oliveira <gabriel.pso@bb.com.br>
2022-07-01 17:35:10 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+Crie uma estrutura de classes para simular o envio de uma mensagem por e-mail, mas, antes de enviar a mensagem, disponibilizar diversos tipos de formatações. A mensagem pode ser enviada com nenhuma, uma ou mais de uma das seguintes formatações
+
+    Formatar tudo em maiúsculo
+    Formatar tudo em minúsculo
+    Formatar em camelCase
+    Remover acentuação
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
